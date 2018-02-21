7a4fc24375d36c36b52d34268d5dac4c8164a0a6
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2018-02-21 22:22:05 +0100

    fixed typos

--- docker/entry.sh
+++ docker/entry.sh
@@ -12,12 +12,13 @@
 #
 #  Nextflow is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
 #  along with Nextflow.  If not, see <http://www.gnu.org/licenses/>.
 
-# enable debuging 
+# enable debugging
 [[ "$NXF_DEBUG_ENTRY" ]] && set -x
 
 # wrap cli args with single quote to avoid wildcard expansion
